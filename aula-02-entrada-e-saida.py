9038ebd8f78ce31aa83681889d25879a27472c3a
wiliami <wiliaup97@gmail.com>
2022-02-04 17:00:47 -0400

    first commit

---
+++ aula-02-entrada-e-saida.py
@@ -0,0 +1,44 @@
+# nome = input('Escreva seu nome: ')
+# idade = input('Escreva a sua idade: ')
+# altura = input('Escreva a sua altura: ')
+# welcome = 'Seja bem-vindo(a)', nome, 'ao Bolsa Família!';
+# print(welcome) # boas vindas ao usuário
+# frase = nome + ' tem ' + str(idade) + ' anos e sua altura é ' + str(altura)
+# print(frase)
+
+
+# operações matemáticas - calculadora
+
+# operadores:
+# adição (+)
+# subtração (-)
+# multiplicação (*)
+# divisão (/)
+# elevado ao quadrado (**)
+# raíz quadrada (1/2)
+
+
+#numero1 = 20
+#numero2 = 20
+#complemento = ' é o resultado da subtração'
+
+#resultado = str(numero1 - numero2) + complemento
+#print(resultado)
+
+
+
+#EXERCICIO: Faça um formulário que pergunte
+#nome, cpf, endereço, idade, altura e telefone
+#
+#e imprima isso em um relatório organizado
+
+
+nome = input('Escreva seu nome: ')
+cpf = input('Digite seu cpf: ')
+endereco = input('Digite seu endereço: ')
+idade = input('Escreva sua idade: ')
+altura = input('Escreva sua altura: ')
+telefone = input('Digite o seu número para contato: ')
+
+relatorio = 'NOME: ',nome, 'CPF: ',cpf, 'ENDERECO: ',endereco, 'IDADE: ',idade, 'ALTURA: ',altura, 'TELEFONE: ',telefone
+print(relatorio)
