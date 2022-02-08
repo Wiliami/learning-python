f849a697b8824d17117ed810975b0b212ab099b4
wiliami <wiliaup97@gmail.com>
2022-02-07 21:20:37 -0400

    atualizando movies e aula 5

---
+++ aula_05_estruturas_de_laco.py
@@ -0,0 +1,37 @@
+#nomes = ['Gustavo Lima', 'Dudu', 'Marcelo', 'João', 'Júlia']
+
+
+### -- estruturas de repetição só entram no loop se for verdadeiro -- ###
+#### ESTRUTURA: FOR #########################################################################
+# for nome in nomes:
+#     print('')
+
+# lista_frutas = ['abacate', 'maça', 'uva', 'abacaxi']
+
+# contador = 0
+
+# for fruta in lista_frutas:
+#     contador += 1
+
+# print(contador)
+
+
+### ESTRUTURA: WHILE ########################################################################
+# i = 0
+
+# while i < 10:
+#     print('i ainda é menor do que 10: ', i)
+#     i = i + 1
+
+# print('Acabou o while: ', i)
+
+
+numero = 0
+
+while True:
+    print(numero)
+    if numero == 20:
+        break
+    numero += 1
+
+print('saiu do while')
--- src/pages/movies.py
+++ src/pages/movies.py
@@ -1,16 +1,18 @@
-movies = ['A era do gelo', 'Nárnia', 'Homem Aranha', 'Deus não está morto']
+movies = ['A era do gelo', 'Nárnia', 'Homem Aranha', 'Deus não está morto', 'Peaky Blinders', 'A era do gelo', 'Senhor dos Anéis', 'Um amor para recordar', 'The Batman'] ## mini banco de dados
+
 pagina = open('movies.html', 'w')
 pagina.write("""
 <!DOCTYPE html>
 <html lang="pt-BR">
     <head>
-        <meta charset=UTF-8>
+        <meta charset="utf-8">
         <title>Lista de filmes com python</title>
     </head>
     <body>
+        <h1>Lista de filmes</h1>\n
 """)
 for movie in movies:
-   print(movie)
+    pagina.write('<li href="/">%s</li>\n' % movie)
 pagina.write("""
     </body>
 </html>
