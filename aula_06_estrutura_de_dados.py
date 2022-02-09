aedc8e50c32d703605fb83966470859b92315809
wiliami <wiliaup97@gmail.com>
2022-02-09 18:39:07 -0400

    finalizando a aula 6

--- aula_06_estrutura_de_dados.py
+++ aula_06_estrutura_de_dados.py
@@ -1,3 +1,24 @@
 minha_lista = ['João', 'Caio'] # lISTA (list)
-minha_tupla = ('João', 'Caio') # TUPLA (tuple)
+minha_tupla = ('João', 'Caio') # TUPLA (tuple) - a tupla númeor definido de coisas
+meu_dicionario = {'nome': 'Wiliamis', 'idade': 24, 'apelido': 'Dudu',} # dicionario (dict)
+meu_conjunto = {'Gui', 'João'} # é uma lista, mas não tem índice
 
+# pra ver se existe um dado em uma coleção eu posso utilizar o DICIONÁRIO ou CONJUNTO
+
+#print(meu_dicionario['nome'])
+
+
+# if 'Wiliamis' in meu_dicionario.values():
+#     print('encontrado')
+
+meu_conjunto.add('Carlos')
+print(meu_conjunto)
+
+lista = []
+tupla = ()
+dicionario = {}
+conjunto = set()
+
+
+loucura = [('carlos', 'fabio'), ('rafael', 'jorge'), ('andré', 'caio'), ({'joao', 'maria'}, {'jorginho', 'lukako'})]
+print(loucura)
