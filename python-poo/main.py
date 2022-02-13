2e3ce24c1071fd92fefcc5f3c71f1941c48c7d47
wiliami <wiliaup97@gmail.com>
2022-02-13 00:13:08 -0400

    aula de poo

--- python-poo/main.py
+++ python-poo/main.py
@@ -1,3 +1,13 @@
 from veiculo import Veiculo
 
-veiculo = Veiculo('rosa', '', 'uno')
+veiculo = Veiculo('rosa', 6, 'uno', 40)
+
+print(veiculo)
+print(type(veiculo))
+
+print('cor:', veiculo.cor)
+print('roda:', veiculo.roda) 
+print('marca:', veiculo.marca)
+print('abastecer:', veiculo.tanque)
+veiculo.abatecer(60)
+print('abastecer:', veiculo.tanque)
--- python-poo/veiculo.py
+++ python-poo/veiculo.py
@@ -1,6 +1,10 @@
 class Veiculo :
 
-    def __init__(self, cor, rodas, marca):
-    self.cor = cor
-    self.roda = roda
-    self.marca = marca
+    def __init__(self, cor, rodas, marca, tanque):
+        self.cor = cor
+        self.roda = rodas
+        self.marca = marca
+        self.tanque = tanque
+
+    def abatecer(self, litros):
+        self.tanque += litros
