41c4bc375eee95a17b444e45ed9c649f579bfd6c
wiliami <wiliaup97@gmail.com>
2022-02-21 07:17:19 -0400

    manipulando arquivos aula 10

---
+++ aula_10_entrada_e_saida_de_arquivos.py
@@ -0,0 +1,14 @@
+#open('arquivo.txt', 'r+')
+#open('arquivo.txt', 'a') # apend (adicionar arquivo)
+#open('arquivo.txt', 'b') # arquivos binários
+#open('arquivo.txt', 'w')
+arquivo = open('image 1.png', 'rb')
+
+# for i in range(1, 1000):
+#     arquivo.write(str(i)+"\n")
+
+
+# for linha in arquivo:
+#     print(linha)
+
+print(arquivo)
