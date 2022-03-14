2b42fa82e33455b624626925dbaf13d9099525d4
wiliami <wiliaup97@gmail.com>
2022-03-14 01:39:45 -0400

    'to-up-to-date'

--- aula_15_clima.py
+++ aula_15_clima.py
@@ -1,8 +1,10 @@
 import requests
 import json
 
+
+requisicao = ''
 cidade = input('Escreva sua cidade: ')
-requisicao = requests.get('api.openweathermap.org/data/2.5/weather?q='+cidade+'&appid=81af18b4bea490ff86824dc7e74c000b')
+requisicao = requests.get('api.openweathermap.org/data/2.5/weather?q='+cidade+'&app id=81af18b4bea490ff86824dc7e74c000b')
 #print(requisicao.text)
 
 tempo = json.loads(requisicao.text)
